db19df18e457023e46898c12d3eadf8595fa58ca
Travis Genovese <travis.genovese@gmail.com>
2019-02-04 19:20:37 -0800

    simplified JavaScript

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,5 @@
 var mymap = L.map('map').setView([32.9928, -117.1859], 10);
 
-// var myIcon = L.icon({
-//   iconUrl: './static/images/',
-//   iconSize: [28, 30],
-//   iconAnchor: [22, 94],
-//   popupAnchor: [-6, -100]
-// });
-
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -18,41 +11,17 @@ d3.json("/api/locations").then(function(data) {
   // console.log(data);
   data.forEach(function(d) {
     // console.log(d);
-    var latsLonsGreen = [];
-    var latsLonsYellow = [];
-    var latsLonsRed = [];
     if (d.Avg_Speed >= 65) {
-      latsLonsGreen.push(d.Latitude, d.Longitude);
+      color = 'green';
     } else if (d.Avg_Speed <= 25) {
-      latsLonsRed.push(d.Latitude, d.Longitude);
+      color = 'red';
     } else { 
-      latsLonsYellow.push(d.Latitude, d.Longitude);
+      color = 'yellow';
     }
-
-    latsLonsGreen.forEach(function() {
-      L.circle(latsLonsGreen, {color: 'green', radius: 100})
-      .addTo(mymap);
-      })
-
-    latsLonsYellow.forEach(function() {
-      L.circle(latsLonsYellow, {color: 'yellow', radius: 100})
-        .addTo(mymap);
-      })
-
-    latsLonsRed.forEach(function() {
-      L.circle(latsLonsRed, {color: 'red', radius: 100})
+    var latsLons = [d.Latitude, d.Longitude];
+    latsLons.forEach(function() {
+      L.circle(latsLons, {color: color, radius: 100})
         .addTo(mymap);
       })
     })
 });
-
-// function getMenu(brewery) {
-//   var rows= d3.select('tbody');
-//   rows.html('');
-//   d3.json(`/menu/${brewery}`).then(function(data) {
-//     data.forEach(function(d) {
-//       rows.append('tr')
-//         .html(`<td>${d.Beer}</td><td>${d.Style}</td><td>${d.ABV}</td><td>${d.Score}</td>`)
-//     })
-//   });
-// }
